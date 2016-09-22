56ca472a3c2196413b3df3a8b003b2cc764da847
Pål Grønås Drange <PGDR@statoil.com>
2016-09-22 15:41:07 +0200

    Old style no ERT support

--- qtgame/qtgame.py
+++ qtgame/qtgame.py
@@ -4,13 +4,6 @@ from PyQt4.QtGui import QApplication
 from snake_game import SnakeGame
 from snake_view import SnakeView
 from snake_model import SnakeModel
-from os.path import isfile
-
-try:
-    from snake_model_norne import SnakeModelNorne
-    from resslice import ResSlice
-except:
-    print('No ERT support')
 
 def printIntro():
     print('Welcome to QtSnake!')
@@ -27,21 +20,11 @@ def main():
     timer  = 60 # ms sleep
     timelimit = 100 # game duration (seconds)
 
-    gridfile = 'norne.EGRID'
-    initfile = 'norne.INIT'
-    if not isfile(gridfile) or not isfile(initfile):
-        print('Needs norne.EGRID and norne.INIT to play norne.')
-
     printIntro()
 
     app = QApplication([])
     view = SnakeView(width, height, size)
-    model = None
-    try:
-        res_slice = ResSlice(gridfile, initfile, kw='SWATINIT')
-        model = SnakeModelNorne(res_slice, width, height)
-    except:
-        model = SnakeModel(width, height)
+    model = SnakeModel(width, height)
     controller = SnakeGame(view, model, app.quit, timelimit)
     controller.run(timer)
 
