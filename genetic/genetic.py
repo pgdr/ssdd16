5c9812f6088aad557713c941b66f0e430ba1491c
Pål Grønås Drange <PGDR@statoil.com>
2016-09-27 17:20:49 +0200

    Added [yet another] ecl support

--- genetic/genetic.py
+++ genetic/genetic.py
@@ -13,13 +13,15 @@ def printIntro():
     print('')
     print('Usage:       genetic.py matrix.m')
     print('Usage:       genetic.py bigmatrix.m')
+    print('Usage:       genetic.py ~/norne/NORNE_ATW2013.EGRID')
+    print('Usage:       genetic.py ~/norne/NORNE_ATW2013.EGRID 11')
     print('')
+    print('Press Space  to show OPT (DP)')
     print('Press Q/Esc  to quit')
 
+
 def main():
-    height = 20
-    width  = 30
-    size   = 14
+    size   = 14 # drawn size of each cell
     timer  = 15 # ms sleep
 
     iters    = 1  # GA iterations per update
@@ -28,13 +30,26 @@ def main():
     apxratio = 0.99
 
     printIntro()
-    if len(sys.argv) != 2:
-        print('Provide matrix file')
+    if len(sys.argv) < 2:
+        print('Provide matrix or EGRID file.')
         exit(1)
 
-    matrix = readMatrix()
+    fname = sys.argv[1]
+    matrix = None
+    if fname[-6:] == '.EGRID':
+        i_slice = 11
+        if len(sys.argv) == 3:
+            i_slice = int(sys.argv[2])
+        matrix = readGrid(fname, i_slice)
+        if not matrix:
+            print('Failed to read ecl files.')
+            exit(1)
+    else:
+        matrix = readMatrix(fname)
+
     width = len(matrix)
     height= len(matrix[0])
+
     app = QApplication([])
     view = GeneticSnakeView(width, height, size)
     model = GeneticSnakeModel(width, height)
@@ -46,13 +61,31 @@ def main():
 
 
 
+def readGrid(fname, i_slice):
+    try:
+        from ert.ecl import EclGrid, EclFile
+        from resslice import getwall
+        from os.path import isfile
+        initfname = fname[:-6] + '.INIT'
+        restfname = fname[:-6] + '.UNRST'
+        init,rest = None, None
+        if isfile(restfname):
+            rest = EclFile(restfname)
+        elif isfile(initfname):
+            init = EclFile(initfname)
+        else:
+            print('Need at least one of INIT and UNRST.')
+            return None
+        grid = EclGrid(fname)
+        return getwall(i_slice, grid, init, rest)
+    except Exception as e:
+        print(e)
+        return None
 
-
-def readMatrix():
-    mfile = sys.argv[1]
+def readMatrix(fname):
     first = True
     matrix = []
-    with open(mfile, 'r') as f:
+    with open(fname, 'r') as f:
         for line in f:
             data = list(map(float, line.strip().split()))
             for i in range(len(data)):
